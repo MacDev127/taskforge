a08554df248f12a80f32470be18d4eefa8bd0312
MacDev <69508380+MacDev127@users.noreply.github.com>
2025-02-13 15:47:24 +0000

    setup api calls for login and register components,

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,7 +1,64 @@
-import React from 'react';
-
+import { useState, useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { Button, TextField, Container, Typography, Link } from '@mui/material';
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await auth?.login(email, password);
+      navigate('/dashboard');
+    } catch (error) {
+      console.error('Login failed', error); // Handle the error here, for example, display an error message.
+    }
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Typography variant="h4">Login</Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Email"
+          variant="filled"
+          fullWidth
+          margin="normal"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          fullWidth
+          margin="normal"
+          variant="filled"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Button type="submit" variant="contained" color="primary">
+            Login
+          </Button>
+          <Link href="/register">
+            {' '}
+            <Typography variant="h6" style={{ fontSize: '12px' }}>
+              Register
+            </Typography>
+          </Link>
+        </div>
+      </form>
+    </Container>
+  );
 };
 
 export default Login;
--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,7 +1,75 @@
 import React from 'react';
+import { useState, useContext } from 'react';
+import axios from 'axios';
+import { AuthContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { Button, TextField, Container, Typography, Link } from '@mui/material';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:5000/api/auth/register', {
+        name,
+        email,
+        password,
+      });
+      navigate('/login');
+    } catch (error) {
+      console.log('registration failed', error);
+    }
+  };
+  return (
+    <Container maxWidth="xs">
+      <Typography variant="h4">Register</Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Name"
+          fullWidth
+          margin="normal"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          label="Email"
+          fullWidth
+          margin="normal"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          fullWidth
+          margin="normal"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Button type="submit" variant="contained" color="primary">
+            Login
+          </Button>
+          <Link href="/login">
+            {' '}
+            <Typography variant="h6" style={{ fontSize: '12px' }}>
+              All Ready Registerd?
+            </Typography>
+          </Link>
+        </div>
+      </form>
+    </Container>
+  );
 };
 
 export default Register;
